c5f0b9e8891efaccac10122f4ab1f5c00407de4b
Md Mashfe Islam <6islam31@solent.ac.uk>
2023-10-20 12:47:31 +0100

    Updated code to display a message with escape characters.

--- output_tasks.py
+++ output_tasks.py
@@ -6,4 +6,10 @@ print("...How to output to the screen")
 print("...How to get user input")
 print("I hope you are enjoying the lesson thus far!")
 
-
+# display the Escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
+print(\n\t  "I am programming!")
